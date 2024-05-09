7ecc21218df40da766686345f9cd8f4c04ae3620
Nicholas Rocca <nicholas.rocca@marconi.loc>
2024-05-09 15:52:14 +0200

    ciao

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World");
+        // creo una repository su git Cd copuio il code apro il terminale in bash 
+        //eseguo il comando gIt init, si fa solo una volta
+        //torniamo su github e prendioamo il coduice get remo0de add 
+        //git add . poi git commit -am ogni volta che facciamo modifiche
+        //poi git push --all
+        
+
+    }
+}
